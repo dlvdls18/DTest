391fe5309bad0e7ba719257a63380ce72366cd6d
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-02 16:07:41 +0800

    update dtest.js

--- src/dtest.js
+++ src/dtest.js
@@ -176,9 +176,11 @@ function DTest(func, rules, handler) {
     var rule = rules[i];
     var rf = [string, randint, bool, any, stringarray, numarray, boolarray, anyarray, stringobject, numobject, boolobject, anyobject];
     var rt = rule.type;
-    var f = arraysearch(rf, rt);
-    if(rule.nullable == true && bool() == true) call_args.push(null);
-    else call_args.push(rule.pred_value || f.apply({}, rule.config || []));
+    if(typeof rt =) var f = arraysearch(rf, rt);
+    if(
+      if(rule.nullable == true && bool() == true) call_args.push(null);
+      else call_args.push(rule.pred_value || f.apply({}, rule.config || []));
+    }
   }
   // call the function directly
   if(handler == null) return {
