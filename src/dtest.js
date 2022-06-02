a85578c96bf11139cec856b69d5fc175ed900e0e
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-02 16:35:19 +0800

    update dtest.js

--- src/dtest.js
+++ src/dtest.js
@@ -189,7 +189,10 @@ function DTest(func, rules, handler, config) {
     else call_args.push(rule.pred_value || f.apply({}, rule.config || []));
   }
   // remove duplicates
-  if(config.remove_duplicates
+  if(config.remove_duplicates == true) {
+    call_args = new Set(call_args);
+    
+  }
   // call the function directly
   if(handler == null) return {
     call_args, func,
