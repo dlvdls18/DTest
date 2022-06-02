bb8e2d8db5366525d47a2c6530953037a3ba27a5
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-02 13:44:25 +0800

    update dtest.js

--- src/dtest.js
+++ src/dtest.js
@@ -19,7 +19,7 @@ function DTest(func, rules, handler) {
   }
   // return random number with range
   function randint(min, max) {
-    min = min || 0;
+    min = min || 0
     max = max || 10;
     return Math.floor(Math.random() * (max + min + 1)) + min;
   }
