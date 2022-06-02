9b57f37c8842c80bff620beda7df39d6c01860a1
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-02 14:47:50 +0800

    update dtest.js

--- src/dtest.js
+++ src/dtest.js
@@ -42,7 +42,7 @@ function DTest(func, rules, handler) {
   function bool(tp, fp) {
     if(!tp || !fp) return randint(0, 1) == 1;
     else {
-      var rand = randint(0, 3);
+      var rand = randint(0, 4);
       if();
     }
   }
