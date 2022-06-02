8fa2bc5955c9e1bb87aa00d0301f0e637fe814ee
Delve Delos Santos Jr <106716453+dlvdls18@users.noreply.github.com>
2022-06-02 15:07:30 +0800

    update dtest.js

--- src/dtest.js
+++ src/dtest.js
@@ -44,7 +44,9 @@ function DTest(func, rules, handler) {
     else {
       if(tp == fp) return randint(0, 1) == 1;
       else if(tp == 0 && fp == 1) return randint(0, 2) == 0;
-      else if(tp == 0 && fp == 2) return randint(p, );
+      else if(tp == 0 && fp == 2) return randint(0, 3) == 0;
+      else if(tp == 1 && fp == 0) return randint(0, 2) != 0;
+      else if(tp == 1 && fp == 2) return randint(0, 3) == 0;
     }
   }
   // return random any type
